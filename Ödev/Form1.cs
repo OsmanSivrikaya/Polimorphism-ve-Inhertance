dba2f6b33c633abe6fd39099fc530c0760371c4a
OsmanSivrikaya <osmansivrikaya@outlook.com>
2022-03-02 01:43:35 +0300

    Polimorphism ve Inhertance

---
+++ Ödev/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Ödev
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=OSMANSIVRIKAYA;Initial Catalog=Northwind;Integrated Security=True");
+        SqlDataAdapter adtr;
+        SqlCommand komut;
+        DataTable tablo = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KayitGoster();
+            dataGridView2.DataSource = KayitGoster2();
+        }
+        public void KayitGoster()
+        {
+            tablo.Clear();
+            adtr = new SqlDataAdapter("spGoster", con);
+            adtr.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+        DataTable KayitGoster2()
+        {
+            con.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Orders order by OrderID desc", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            return dt;
+        }
+
+        TelefonSatis ts = new TelefonSatis();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            ts.ProductName = textBox1.Text;
+            ts.SupplersID = Convert.ToInt32(textBox2.Text);
+            ts.CategoryID = Convert.ToInt32(textBox3.Text);
+            ts.UnitsInStock = Convert.ToInt32(textBox4.Text);
+            ts.UnitPrice = Convert.ToDouble(textBox5.Text);
+            Suppliers s = new Suppliers();
+            s.YeniKayitEkle();
+            ts.YeniKayitEkle();
+            Form1_Load(this, null);
+            //KayitGoster();
+        }
+
+       
+    }
+}
---
+++ Ödev/Suppliers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Ödev
+{
+
+    public class Suppliers
+    {
+        public int UnitsInStock { get; set; }
+        public int SupplersID { get; set; }
+        public virtual void YeniKayitEkle()
+        {
+            SqlConnection con = new SqlConnection("Data Source = OSMANSIVRIKAYA; Initial Catalog = Northwind; Integrated Security = True");
+            //con.ConnectionString = connection;
+            SqlDataAdapter adtr;
+            SqlCommand komut;
+            DataTable tablo = new DataTable();
+            TelefonSatis ts = new TelefonSatis();
+            con.Open();
+            komut = new SqlCommand("spYeniKayıt", con);
+            komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("@OrderDate", DateTime.Now.ToString());
+            komut.ExecuteNonQuery();
+            con.Close();
+        }
+        
+    }
+    public class TelefonSatis : Suppliers
+    {
+        public double UnitPrice { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryID { get; set; }
+        public override void YeniKayitEkle()
+        {
+            SqlConnection con = new SqlConnection("Data Source=OSMANSIVRIKAYA;Initial Catalog=Northwind;Integrated Security=True");
+            SqlDataAdapter adtr;
+            SqlCommand komut;
+            DataTable tablo = new DataTable();
+            con.Open();
+            komut = new SqlCommand("psYeniKullaniciEkle", con);
+            komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("@ProductName", ProductName);
+            komut.Parameters.AddWithValue("@SupplierID", SupplersID);
+            komut.Parameters.AddWithValue("@CategoryID", CategoryID);
+            komut.Parameters.AddWithValue("@UnitsInStock", UnitsInStock);
+            komut.Parameters.AddWithValue("@UnitPrice", UnitPrice);
+            komut.ExecuteNonQuery();
+            con.Close();
+        }
+       
+    }
+}
